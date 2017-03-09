c5cbf2f5e0ceb95423c2713539da992e1011e1ff
zwei <zwei@adobe.com>
2017-03-09 14:24:32 -0800

    update git layers

--- z_layers.py
+++ z_layers.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import numpy as np
-
+import tensorflow.contrib.slim as slim
 
 # spp layer
-def spp_layer(input_, levels=None, scope='SPP_layer', reuse=None):
+def spp_layer(input_, levels=None, scope='spp_layer', reuse=None):
     if not levels:
         levels = [1, 2, 3]
     shape = input_.get_shape().as_list()
@@ -18,3 +18,22 @@ def spp_layer(input_, levels=None, scope='SPP_layer', reuse=None):
             pool_outputs.append(tf.reshape(pool, [shape[0], -1]))
         spp_pool = tf.concat(axis=1, values=pool_outputs)
     return spp_pool
+
+
+# spp layer in visual finding
+def spp_vfn(input_, levels=None, scope='spp_vfn_layer', reuse=None):
+    if not levels:
+        levels = [3, 5, 7]
+    shape = input_.get_shape().as_list()
+    with tf.variable_scope(name_or_scope=scope, reuse=reuse):
+        pool_outputs = []
+        for l in levels:
+            pool = tf.nn.max_pool(input_, ksize=[1, l, l, 1],
+                                  strides=[1, l-1, l-1, 1],
+                                  padding='VALID')
+            pool_outputs.append(tf.reshape(pool, [shape[0], -1, shape[3]]))
+        spp_pool = tf.concat(axis=1, values=pool_outputs)
+        spp_pool = tf.reduce_max(spp_pool, reduction_indices=[1])
+        tf_reduction = slim.fully_connected(spp_pool, 1000, activation_fn=None,
+                                        normalizer_fn=None, scope='spp_reduction', reuse=False)
+    return tf_reduction
