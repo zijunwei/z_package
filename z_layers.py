09bfcbb0ee068190c30d6e559b3203b3e7766dfc
zwei <zwei@adobe.com>
2017-03-14 22:38:51 -0700

    update layers

--- z_layers.py
+++ z_layers.py
@@ -10,10 +10,10 @@ def spp_layer(input_, levels=None, scope='spp_layer', reuse=None):
     with tf.variable_scope(name_or_scope=scope, reuse=reuse):
         pool_outputs = []
         for l in levels:
-            pool = tf.nn.max_pool(input_, ksize=[1, np.ceil(shape[1] * 1. / l).astype(np.int32),
+            pool = tf.nn.avg_pool(input_, ksize=[1, np.ceil(shape[1] * 1. / l).astype(np.int32),
                                                  np.ceil(shape[2] * 1. /l).astype(np.int32), 1],
-                                  strides=[1, np.floor(shape[1] * 1. / l + 1).astype(np.int32),
-                                           np.floor(shape[2] * 1. /l + 1), 1],
+                                  strides=[1, np.ceil(shape[1] * 1. / l).astype(np.int32),
+                                           np.ceil(shape[2] * 1. /l), 1],
                                   padding='SAME')
             pool_outputs.append(tf.reshape(pool, [shape[0], -1]))
         spp_pool = tf.concat(axis=1, values=pool_outputs)
